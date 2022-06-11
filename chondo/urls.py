c018065ecaa9f0c1ec1481c23c47b503ea476125
rbsamiur <rbsamiur@gmail.com>
2022-06-12 02:13:42 +0600

    admin panel css loaded (updated2)

--- chondo/urls.py
+++ chondo/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
                   path('api/', include('notification.urls')),
                   path('api/', include('note.urls')),
                   path('api/', include('symptoms.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
