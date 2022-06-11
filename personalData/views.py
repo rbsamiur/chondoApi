7e4db6245acd90321cee7ef79364fb8ef4ba1d96
rbsamiur <rbsamiur@gmail.com>
2022-06-11 17:36:52 +0600

    Personal Data creation restricted after once

--- personalData/views.py
+++ personalData/views.py
@@ -8,35 +8,42 @@ from rest_framework import generics
 from .models import PersonalData
 from .serializer import PersonalDataSerializers
 
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getPersonalData(request):
-    user=request.user
-    stored_data=user.personaldata_set.all()
-    serializer= PersonalDataSerializers(stored_data,many=True)
+    user = request.user
+    stored_data = user.personaldata_set.all()
+    serializer = PersonalDataSerializers(stored_data, many=True)
     return Response(serializer.data)
 
-# todo: need to find if a data exist of not
+
+
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def createPersonalData(request):
     user = request.user
-    serializer = PersonalDataSerializers(data=request.data)
-    if serializer.is_valid():
-        serializer.save(user=user)
-    return Response(serializer.data)
+    if PersonalData.objects.filter(user=user).exists():
+        return Response("Personal Date already entered. Please use update for change")
+    else:
+        serializer = PersonalDataSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=user)
+        return Response(serializer.data)
+
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def updatePersonalData(request):
-    user =request.user
+    user = request.user
     stored_data = PersonalData.objects.get(user=user)
     print(stored_data)
     serializer = PersonalDataSerializers(instance=stored_data, data=request.data)
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
 
 @api_view(['DELETE'])
 @permission_classes([IsAuthenticated])
